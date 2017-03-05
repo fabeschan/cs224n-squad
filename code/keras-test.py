3c6f1391e9d4bb3c8c23587a71fbdc39a2e11d82
siyue wu <siyuewu@siyues-MacBook.local>
2017-03-04 18:10:12 -0800

    change keras-test

--- code/keras-test.py
+++ code/keras-test.py
@@ -1,6 +1,7 @@
 # LSTM and CNN for sequence classification in the IMDB dataset
 import os, sys
 import numpy as np
+import matplotlib.pyplot as plt
 
 from keras.datasets import imdb
 from keras.models import Sequential
@@ -41,42 +42,116 @@ source_dir = os.path.join("data", "squad")
 def loaddata():
     with open(source_dir + '/train.ids.context', 'r') as f:
         train_context = []
+        train_context_lengthlist = []
         for line in f:
             int_list = [int(x) for x in line.split()]
+            train_context_lengthlist.append(len(int_list))
             train_context.append(int_list)
 
     with open(source_dir + '/train.ids.question', 'r') as f:
         train_question = []
+        train_question_lengthlist = []
         for line in f:
             int_list = [int(x) for x in line.split()]
+            train_question_lengthlist.append(len(int_list))
             train_question.append(int_list)
 
     with open(source_dir + '/train.span', 'r') as f:
         train_span = []
+        train_span_lengthlist = []
+        count = 0
         for line in f:
             val = [int(x) for x in line.split()]
-            #res = int_list[0]*max_length_context + int_list[1]
-            res = n_classes - (max_length_context - (val[0]-1))*(max_length_context-(val[0]-1)+1)/2 + (val[1]-val[0]+1)
+            train_span_lengthlist.append(val[1] - val[0] + 1)
+            res = np.zeros(train_context_lengthlist[count])
+            res[val[0]:val[1] + 1]  = 1
             train_span.append(res)
+            count += 1
+    return[train_context, train_question, train_span, train_context_lengthlist, train_question_lengthlist, train_span_lengthlist]
+'''
+try to plot histogram 
+'''
+if __name__ == '__main__':
+    datas = loaddata()
+    train_context_lengthlist = datas[3]
+    plt.hist(train_context_lengthlist)
+    #plt.axis([0, 42, 50000, 85000])
+    plt.xlabel('train_context_lengthlist')
+    plt.ylabel('Frequency')
+    plt.title('Train_context length Histogram')
+    plt.grid(True)
+    output_path = "{}.png".format("context")
+    plt.savefig(output_path)
 
-    return[train_context, train_question, train_span]
+    train_question_lengthlist = datas[4]
+    plt.hist(train_question_lengthlist)
+    #plt.axis([0, 42, 50000, 85000])
+    plt.xlabel('train_question_lengthlist')
+    plt.ylabel('Frequency')
+    plt.title('Train_question length Histogram')
+    plt.grid(True)
+    output_path = "{}.png".format("question")
+    plt.savefig(output_path)
+
+    train_span_lengthlist = datas[5]
+    plt.hist(train_span_lengthlist)
+    #plt.axis([0, 42, 50000, 85000])
+    plt.xlabel('train_span_lengthlist')
+    plt.ylabel('Frequency')
+    plt.title('Train_span length Histogram')
+    plt.grid(True)
+    output_path = "{}.png".format("span")
+    plt.savefig(output_path)
 
 
 def padding(datalist):
     train_context = sequence.pad_sequences(datalist[0], maxlen=max_length_context, value=0.)
     train_question = sequence.pad_sequences(datalist[1], maxlen=max_length_question, value=0.)
-    return [train_context, train_question]
+    train_span = sequence.pad_sequences(datalist[2], maxlen=max_length_context, value=0.)
+    return [train_context, train_question, train_span]
 
 
 if __name__ == '__main__':
     datas = loaddata()
     padded_data = padding(datas)
 
+    '''
+    try to plot the histogram of length
+    '''
+    train_context_lengthlist = datas[3]
+    plt.hist(train_context_lengthlist)
+    #plt.axis([0, 42, 50000, 85000])
+    plt.xlabel('train_context_lengthlist')
+    plt.ylabel('Frequency')
+    plt.title('Train_context length Histogram')
+    plt.grid(True)
+    output_path = "{}.png".format("context")
+    plt.savefig(output_path)
+
+    train_question_lengthlist = datas[4]
+    plt.hist(train_question_lengthlist)
+    #plt.axis([0, 42, 50000, 85000])
+    plt.xlabel('train_question_lengthlist')
+    plt.ylabel('Frequency')
+    plt.title('Train_question length Histogram')
+    plt.grid(True)
+    output_path = "{}.png".format("question")
+    plt.savefig(output_path)
+
+    train_span_lengthlist = datas[5]
+    plt.hist(train_span_lengthlist)
+    #plt.axis([0, 42, 50000, 85000])
+    plt.xlabel('train_span_lengthlist')
+    plt.ylabel('Frequency')
+    plt.title('Train_span length Histogram')
+    plt.grid(True)
+    output_path = "{}.png".format("span")
+    plt.savefig(output_path)
 
     # create the model
     embedding_vecor_length = 32
     model = Sequential()
-    model.add(Embedding(input_dim=115613, output_dim=128, input_length=max_length_question))
+    model.add(Embedding(input_dim=glove_size, output_dim=128, input_length=max_length_question))
     model.add(LSTM(100))
     model.add(Dense(output_dim=max_length_context, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
@@ -85,14 +160,8 @@ if __name__ == '__main__':
     #combined_input = np.concatenate(padded_data, axis = 1)
     #now combined_input includes only questions
     combined_input = padded_data[1]
-
-    output = np.zeros(max_length_context)
-    span = datas[2]
-    output[span[0]: span[1]] = 1
+    model.fit(combined_input, padded_data[2], nb_epoch=10, batch_size=128, validation_split=0.1)
     
-    classed_context = np_utils.to_categorical(output, nb_classes=n_classes)
-    model.fit(combined_input, classed_context, nb_epoch=10, batch_size=128, validation_split=0.1)
-
     '''
     # Final evaluation of the model
     scores = model.evaluate(X_test, y_test, verbose=0)
